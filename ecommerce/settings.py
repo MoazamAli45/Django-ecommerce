74c49190720c3bff141d1de7ab962168c7802130
MoazamAli45 <smoazam987@gmail.com>
2025-07-09 23:44:07 +0500

    Update store title and headings to reflect IOT branding; adjust relevant HTML templates.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -153,7 +153,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/5.2/ref/settings/Default#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
